d064f717773055c0fc2f136c488fa160e88c36d6
Kevin Groot <groot.ks@gmail.com>
2015-10-12 01:02:43 +0200

    Test Commit

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MyFirstOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MyFirstOp.java
@@ -1,15 +1,19 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by Kevin on 11/10/2015.
  */
 public class MyFirstOp extends OpMode
 {
+
+    DcMotor rightMotor;
+
     public void init()
     {
-
+        rightMotor = hardwareMap.dcMotor.get("motor_1");
     }
 
     public void loop()
