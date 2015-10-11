d7edb2add2bab36db9d812dd15ba34a0aa1a204f
Kevin Groot <groot.ks@gmail.com>
2015-10-11 23:05:19 +0200

    added MyFirstOp

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MyFirstOp.java
@@ -0,0 +1,19 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Kevin on 11/10/2015.
+ */
+public class MyFirstOp extends OpMode
+{
+    public void init()
+    {
+
+    }
+
+    public void loop()
+    {
+
+    }
+}
